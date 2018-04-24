eff7d229d51bc663509bda61cef8b7ede51c1e69
vivian1315 <1597933676@qq.com>
2018-04-24 16:15:51 +0800

    cnck

---
+++ Vivian website_4.10/index.php
@@ -0,0 +1,141 @@
+<?php include_once("index.html"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+<title>Vivian's website</title>
+<!--  Bootstrap Style -->
+<link href="assets/css/bootstrap.css" rel="stylesheet" />
+<!--  Font-Awesome Style -->
+
+<!--  Animation Style -->
+<!--<link href="assets/css/animate.css" rel="stylesheet" />-->
+<!--  一些关于文字等的旋转动画 -->
+<!--  Pretty Photo Style -->
+<!--<link href="assets/css/prettyPhoto.css" rel="stylesheet" />-->
+<!--  Google Font Style -->
+<!--<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />-->
+<!--  Custom Style -->
+<link href="assets/css/style.css" rel="stylesheet" />
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--  有关表格的css -->
+
+
+</head>
+<body>
+
+<!--/. PRELOADER END -->
+<div class="navbar navbar-default navbar-fixed-top move-me ">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+
+			<a class="navbar-brand" href="#">
+				<img src="assets/img/logo.png" class="navbar-brand-logo " alt="" />
+
+			</a>
+		</div>
+		<!-- 最上栏导航栏 -->
+		<div class="navbar-collapse collapse move-me">
+			<ul class="nav navbar-nav navbar-right">
+
+	<li >
+					<a href="#home">Time Flower 
+
+					</a>
+
+				</li>
+
+
+			
+				<li >
+					<a href="#about">About Me
+
+					</a>
+
+				</li>
+
+				<li >
+					<a href="#port-folio">Hobbies picture
+					</a>
+
+
+				</li>
+
+				<li >
+					<a href="#contact">Contact Me
+					</a>
+
+				</li>
+
+			</ul>
+		</div>
+
+	</div>
+</div>
+
+
+<!-- define “About Me” designer container -->
+				<div id="about" >
+	<div class="container">
+		<div class="row text-center">
+			<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 sub-head">
+				<h2  data-wow-delay="0.3s" class="wow rollIn animated" ><strong> About Me </strong></h2>
+				<p class="sub-head  wow rotateIn animated" data-wow-delay="0.4s"><strong>My Address :</strong> Siva , Shong Jiang University Town, Shanghai of China </p>
+			</div>
+				</div>
+		</div>
+		</div>
+
+<div id="port-folio" class="pad-top-botm" >
+	<div class="container">
+		<div class="row text-center ">
+			<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
+				<h2><strong> Hobbies picture </strong></h2>
+				<p class="sub-head">Vivian‘s hobby ........ </p>
+				
+			</div>
+		</div>
+		</div>
+		</div>
+
+		<!--./ NAV BAR END -->
+<!-- define contact designer container -->
+		<div id="home" >
+	<div class="overlay">
+		<div class="container">
+			<div class="row ">
+				<div class="col-lg-9 col-md-9 head-text">
+					<h1 id="divDisp" >Contact Me at social media！</h1>
+					<span >
+						<i class="fa fa-lightbulb-o " ></i>Email ： 
+					</span>
+					<span>
+						<i class="fa fa-lightbulb-o" ></i>Wechat ： 
+					</span>
+					
+					<span >
+						<i class="fa fa-lightbulb-o" ></i>Blog ： 
+					</span>
+					<span >
+						<i class="fa fa-lightbulb-o" ></i>github ：
+					</span>
+					
+						</div>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+
+
+
+</body>
+</html>
